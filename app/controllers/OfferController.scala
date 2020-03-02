9c1d9de4a669812ac0146f7e60fac4fd3adb0392
Jianfeng Tian <jianfeng.tian@sky.uk>
2020-03-02 12:51:03 +0000

    introduce OfferRepo

--- app/controllers/OfferController.scala
+++ app/controllers/OfferController.scala
@@ -8,16 +8,24 @@ import play.api.libs.circe.Circe
 import io.circe.generic.auto._
 import io.circe.syntax._
 import models.Offers.{Offer, OfferId, OfferWithId}
-import models.Responses.CreateOfferResponse
+import models.Responses.{CreateOfferFailureResponse, CreateOfferResponse}
 
+import scala.collection.concurrent.TrieMap
 import scala.concurrent.{ExecutionContext, Future}
 
 class OfferController @Inject()(val cc: ControllerComponents)(implicit exec: ExecutionContext)  extends AbstractController(cc) with Circe {
 
+  val offerRepo = new TrieMap[OfferId, Offer]
+
   def create = Action.async(circe.json[Offer]) { implicit request =>
     val offer = request.body
+
     val offerId = OfferId(UUID.randomUUID())
-    Future.successful(Created(CreateOfferResponse(OfferWithId(offerId, offer)).asJson))
+    offerRepo += ((offerId, offer))
+    offerRepo.get(offerId) match {
+      case None => Future.successful(InternalServerError(CreateOfferFailureResponse(offer = offer).asJson))
+      case Some(retrievedOffer) => Future.successful(Created(CreateOfferResponse(offerWithId = OfferWithId(offerId, retrievedOffer)).asJson))
+    }
   }
 
 }
--- app/models/Responses.scala
+++ app/models/Responses.scala
@@ -3,5 +3,10 @@ package models
 import models.Offers.{Offer, OfferWithId}
 
 object Responses {
-  case class CreateOfferResponse(offerWithId: OfferWithId)
+
+  sealed trait OfferResponse {
+    def msg: String
+  }
+  case class CreateOfferResponse(msg: String = "An offer is created", offerWithId: OfferWithId) extends OfferResponse
+  case class CreateOfferFailureResponse(msg: String = "Failed to create an offer", offer: Offer) extends OfferResponse
 }
